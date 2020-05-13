124be57ed2971847901941f11fd1e1a0acbbb2a1
Emanoel Da Costa Oliveira <emanoeldacostaoliveira@outlook.com>
2020-05-13 19:27:45 -0300

    Atualização de API em relação ao uso de tokkens e contas

--- server.py
+++ server.py
@@ -3,14 +3,22 @@ from totalvoice.cliente import Cliente
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET','POST'])
+#Para utilizar a aplicação deve ser criada uma conta no totalvoice e pegar a key gerada.
+
+@app.route("/", methods=['POST'])
 def enviar_sms():
     celular = request.form.get('numero')
     texto = request.form.get('mensagem')
-    cliente = Cliente("883737a6404a138640e5e25ea916516c", 'https://api2.totalvoice.com.br/sms')
+    cliente = Cliente("insira sua key para utilizar a aplicação", 'https://api2.totalvoice.com.br/sms')
     response = cliente.sms.enviar(celular,texto)
     print(response)
     return render_template('index.html')
+
+@app.route("/", methods=['GET'])
+def mostrar_tela():
+    return render_template ('index.html')
+
+
     
     
 
--- total_voice(backup).py
+++ total_voice(backup).py
@@ -1,8 +1,8 @@
 from totalvoice.cliente import Cliente
 
-cliente = Cliente("9b691b4f5ebfe5e32d28186be41965e8", 'https://api2.totalvoice.com.br/sms')
+cliente = Cliente("coloque sua key", 'https://api2.totalvoice.com.br/sms')
 
-'''numero_destino = "11967716409"
+'''numero_destino = "11111111111"
 mensagem = "teste envio sms"'''
 numero_destino = input('Digite o numero:\n')
 mensagem = input('Digite a mensagem solicitada:\n')
